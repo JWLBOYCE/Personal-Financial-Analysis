45659c4f89ad612d04a6b4d4220b3ec2b4162c93
JWLBOYCE <39141446+JWLBOYCE@users.noreply.github.com>
2025-06-30 21:38:42 +0100

    Add recurring transactions management tab

--- gui/__init__.py
+++ gui/__init__.py
@@ -4,6 +4,7 @@ from .main_window import MainWindow
 from .login_window import LoginWindow
 from .monthly_tabbed_window import MonthlyTabbedWindow
 from .dashboard_tab import DashboardTab
+from .recurring_tab import RecurringTab
 from .navigation_table_widget import NavigationTableWidget
 
 __all__ = [
@@ -11,5 +12,6 @@ __all__ = [
     "LoginWindow",
     "MonthlyTabbedWindow",
     "DashboardTab",
+    "RecurringTab",
     "NavigationTableWidget",
 ]
